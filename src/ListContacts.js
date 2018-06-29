2520b80e181a4b734f54f986d2c2a20d05ea11b2
Eric Chinen <mik.chinen@gmail.com>
2018-06-29 19:27:08 -0300

    Adicionando propTypes no componente.

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 /*class ListContacts extends Component {
     render() {
@@ -41,6 +42,12 @@ function ListContacts(props) {
             ))}
         </ol>
     )
+}
+
+ListContacts.propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired
+
 }
 
 export default ListContacts
